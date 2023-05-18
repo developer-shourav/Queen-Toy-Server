b0202559424f1458bfc6158cc578c467f07616e5
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-18 14:25:37 +0600

    express server primary template added

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000 ;
+
+const app = express();
+
+
+
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/' , (req, res) => {
+    res.send('Welcome to Our Queen Toy Server')
+})
+
+app.get('/about' , (req, res) => {
+    res.send('This is our About Route')
+})
+
+
+
+app.listen(port , () => {
+    console.log(`Our Awesome Queen Toy is running On the PORT:${port}`);
+})
