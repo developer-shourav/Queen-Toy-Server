e573528ef76e8b25430d0dbd058c05dcc3999860
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-23 21:23:29 +0600

    all toys added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000 ;
 
 const app = express();
@@ -10,6 +12,44 @@ const app = express();
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.evuna6q.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+
+      const toysCollection = client.db('toysDB').collection('allToys');
+      const blogCollection = client.db('toysDB').collection('blogs');
+
+      app.get('/toys', async(req, res) => {
+        const coffees = toysCollection.find();
+        const result =  await coffees.toArray();
+        res.send(result)
+      })
+  
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      /* await client.close(); */
+    }
+  }
+  run().catch(console.dir);
+  
 
 
 app.get('/' , (req, res) => {
